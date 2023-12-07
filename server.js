2a3b38f3c2e4fa3b2fd1665bf48aa9d4a31cc1c9
Stefano Gargiulo <s.gargiulo@jdk.it>
2023-12-07 10:09:23 +0100

    test

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ router.get("/api", async (ctx) => {
 
 
     const appDefinitions = await axios.get(
-      `${caproverUrl}api/v2/user/${appName}`,
+      `${caproverUrl}api/v2/user/assp/${appName}`,
       {
         headers: { "x-captain-auth": token },
         timeout: 2000,
