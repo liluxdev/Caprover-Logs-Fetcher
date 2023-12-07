cd07bfb8291b18a3f37d74ee4a5d1345ffd20bf0
Stefano Gargiulo <s.gargiulo@jdk.it>
2023-12-07 06:28:04 +0100

    logs

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ router.get("/api", async (ctx) => {
 
     ctx.body = {
       isAppBuilding: data.isAppBuilding,
-      logs: "",
+      logs: logs,
       buildLogs: data.logs.lines.join("\n"),
       buildLogDebug: data.logs.lines,
       allowedApps,
