a991b9420d6846436f9a2ee29ba2c951ef679118
Stefano Gargiulo <s.gargiulo@jdk.it>
2023-12-07 05:59:22 +0100

    koa app

--- server.js
+++ server.js
@@ -1,153 +1,85 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
+const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
+const axios = require('axios');
+const path = require('path');
+
+const app = new Koa();
+const router = new Router();
 const port = process.env.PORT || 3000;
 const caproverUrl = process.env.CAPROVER_URL;
 const caproverPassword = process.env.CAPROVER_PASSWORD;
 const allowedApps = process.env.ALLOWED_APPS.split(",");
-const SECRTET = process.env.SECRET;
+const SECRET = process.env.SECRET;
 const MAX_LOG_CHARS = process.env.MAX_LOG_CHARS || 18 * 1000;
-app.use(express.static("./static/"));
 
-app.get("/logs", (req, res) => {
-  res.sendFile(__dirname + "/static/index.html");
+app.use(serve("./static/"));
+
+router.get('/logs', async (ctx) => {
+  ctx.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
-app.get("/api", async (req, res) => {
-  res.set("Content-Type", "application/json"); // Set JSON response header
+router.get('/api', async (ctx) => {
   try {
-    //check secret
-    const secret = req.query.secret;
+    const secret = ctx.query.secret;
     if (!secret) {
-      // return res.status(400).send('Il parametro "secret" è obbligatorio');
-      return res
-        .status(400)
-        .send(
-          JSON.stringify({ error: 'Il parametro "secret" è obbligatorio' })
-        );
+      ctx.status = 400;
+      ctx.body = { error: 'Il parametro "secret" è obbligatorio' };
+      return;
     }
-    if (secret !== SECRTET) {
-      //  return res.status(400).send('Il parametro "secret" non è valido');
-      return res
-        .status(400)
-        .send(JSON.stringify({ error: 'Il parametro "secret" non è valido' }));
+    if (secret !== SECRET) {
+      ctx.status = 400;
+      ctx.body = { error: 'Il parametro "secret" non è valido' };
+      return;
     }
-    const appName = req.query.appName;
+
+    const appName = ctx.query.appName;
     if (!appName) {
-      return res
-        .status(400)
-        .send(
-          JSON.stringify({ error: 'Il parametro "appName" è obbligatorio' })
-        );
+      ctx.status = 400;
+      ctx.body = { error: 'Il parametro "appName" è obbligatorio' };
+      return;
     }
     if (!allowedApps.includes(appName)) {
-      return res
-        .status(400)
-        .send(JSON.stringify({ error: 'Il parametro "appName" non è valido' }));
+      ctx.status = 400;
+      ctx.body = { error: 'Il parametro "appName" non è valido' };
+      return;
     }
+
     console.log("Recuperando i log da CapRover", appName);
-    // Ottenere token da CapRover
-    const tokenResponse = await axios.post(
-      `${caproverUrl}api/v2/login`,
-      {
-        password: caproverPassword,
-      },
-      {
-        timeout: 2000, // Timeout impostato a 5000 millisecondi
-      }
-    );
+    const tokenResponse = await axios.post(`${caproverUrl}api/v2/login`, { password: caproverPassword }, { timeout: 2000 });
     const token = tokenResponse.data.data.token;
 
-    // Recupera i log usando l'API di CapRover
-    const logsResponse = await axios.get(
-      `${caproverUrl}api/v2/user/apps/appData/${appName}/logs?encoding=hex&limit=1000`,
-      {
-        headers: { "x-captain-auth": token },
-      //  encoding: "hex",
-      },
-      {
-        timeout: 2000, // Timeout impostato a 5000 millisecondi
-      }
-    );
-
-    console.log("logsResponse", JSON.stringify(Object.keys(logsResponse.data).length));
+    const logsResponse = await axios.get(`${caproverUrl}api/v2/user/apps/appData/${appName}/logs?encoding=hex&limit=1000`, { headers: { "x-captain-auth": token }, timeout: 2000 });
     let logs = logsResponse.data.data.logs;
     let originalLogLen = logs.length;
     logs = logs.length > MAX_LOG_CHARS ? logs.slice(-MAX_LOG_CHARS) : logs;
     let responseLogLen = logs.length;
 
-    console.log("hex logs", logs.length, originalLogLen);
-
     let buffer = Buffer.from(logs, 'hex');
-
-// Convertire il Buffer in una stringa UTF-8
     let utf8String = buffer.toString('utf8');
 
-    console.log(
-      "utf8String",
-       utf8String.length,
-       utf8String
-    );
-    console.log(
-      "logsResponse",
-      JSON.stringify(Object.keys(logsResponse.data).length)
-    );
-
-    // Get build logs from CapRover API
-    const buildLogsResponse = await axios.get(
-      `${caproverUrl}api/v2/user/apps/appData/${appName}`,
-      {
-        headers: { "x-captain-auth": token },
-      //  encoding: "hex",
-      },
-      {
-        timeout: 2000, // Timeout impostato a 5000 millisecondi
-      }
-    );
-    console.log(
-      "buildLogsResponse",
-      JSON.stringify(Object.keys(buildLogsResponse.data).length)
-    );
+    const buildLogsResponse = await axios.get(`${caproverUrl}api/v2/user/apps/appData/${appName}`, { headers: { "x-captain-auth": token }, timeout: 2000 });
     const { data } = buildLogsResponse.data;
-    const response = {
+
+    ctx.body = {
       isAppBuilding: data.isAppBuilding,
-      logs: logs,//.split("\n").slice(-27).join("\n"),
+      logs: utf8String,
       buildLogs: data.logs.lines.slice(-27).join("\n"),
-     // appData: data,
       allowedApps,
       isBuildFailed: data.isBuildFailed,
       originalLogLen,
       responseLogLen,
     };
-
-/*     console.log(
-      "response",
-      JSON.stringify(response)
-    ); */
-
-    console.log(
-      "sending response for " + appName,
-      JSON.stringify(Object.keys(response).length),
-    //  response
-    );
-    res.send(response);
-    //console.log("sending response json",response);
   } catch (error) {
     console.trace(error);
-    res
-      .status(500)
-      .send(
-        JSON.stringify({
-          error: "Errore nel recuperare i log: " + error.message,
-        })
-      );
+    ctx.status = 500;
+    ctx.body = { error: "Errore nel recuperare i log: " + error.message };
   }
 });
 
-//const compression = require('compression');
-//app.use(compression());
+app.use(router.routes()).use(router.allowedMethods());
 
 const server = app.listen(port, () => {
-  console.log(`Server in ascolto sulla porta ${port}`);
+  console.log(`Server Koa in ascolto sulla porta ${port}`);
 });
-server.timeout = 5000; // Imposta un timeout personalizzato, es. 500000 ms (500 secondi)
+server.timeout = 5000; // Imposta un timeout personalizzato
